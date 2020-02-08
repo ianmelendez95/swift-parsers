6155cb93ef9393e37ea90f623083e99228605663
Ian Melendez <ian.melendez.95@utexas.edu>
2020-02-08 13:33:12 -0600

    bind

--- Parser.swift
+++ Parser.swift
@@ -1,6 +1,30 @@
 typealias Parser<A> = ((String) -> (A, String)?)
 
-func sequence<A>(_ parser1: @escaping Parser<A>, _ parser2: @escaping Parser<A>) -> Parser<A> {
+func bind<A,B>
+(_ parser: @escaping Parser<A>, _ bindFunction: @escaping ((A) -> Parser<B>)) 
+-> Parser<B> {
+  return { input in 
+    if let (result, rest) = parser(input) {
+      return bindFunction(result)(rest)
+    } else {
+      return nil
+    }
+  }
+}
+
+func fmap<A,B>(_ parser: @escaping Parser<A>, _ fmapFunction: @escaping ((A) -> B)) 
+-> Parser<B> {
+  return { input in
+    if let (result, rest) = parser(input) {
+      return (fmapFunction(result), rest)
+    } else {
+      return nil
+    }
+  }
+}
+
+func sequence<A,B>(_ parser1: @escaping Parser<A>, _ parser2: @escaping Parser<B>) 
+-> Parser<B> {
   return { input in
     if let (_, rest1) = parser1(input) {
       return parser2(rest1)
@@ -42,4 +66,17 @@ func testSequenced() {
   let sequenced = sequence(hParser, eParser)
   
   print(sequenced("hello")) // '("e", "llo")'
+}
+
+func testBind() {
+  let hParser = char("h")
+  let eParser = char("e")
+  let bound = bind(hParser, 
+    { (letter: Character) in 
+      fmap(eParser, { (result: Character) in String(letter) + String(result) })
+    })
+
+  let parsed = bound("hello")
+
+  print(parsed)
 }
