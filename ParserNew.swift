40d15d3c0c40bc3b64b66d8642a6ad36ce11d952
Ian Melendez <ian.melendez.95@utexas.edu>
2020-02-09 14:59:04 -0600

    implemented enough in ParserNew for tag attribute parser

--- ParserNew.swift
+++ ParserNew.swift
@@ -1,3 +1,21 @@
+let tagAttribute: Parser<(String, String)> = {
+  let key = Parsers.letters().token()
+
+  let equals = Parsers.char("=")
+  
+  let valueContent = 
+    Parsers.asString(Parsers.satisfy({ c in c != "\"" }).many())
+  let value = valueContent.quoted()
+
+  return key.flatMap({ keyStr in 
+            equals.then(value.map({ valueStr in (keyStr, valueStr) }))
+          })
+}()
+
+func testTagAttribute() { 
+  print(tagAttribute.parse("type=\"text\""))
+}
+
 typealias ParserFunction<A> = ((String) -> (A, String)?)
 
 class Parser<A> {
@@ -10,9 +28,93 @@ class Parser<A> {
   func parse(_ input: String) -> (A, String)? {
     return self.parseFunc(input)
   }
+
+  func quoted() -> Parser<A> {
+    let quote = Parsers.char("\"")
+    return quote.then(self).precedes(quote)
+  }
+
+  func then<B>(_ parser: Parser<B>) -> Parser<B> {
+    return Parser<B>({ input in
+      if let (_, rest1) = self.parse(input) {
+        return parser.parse(rest1)
+      } else {
+        return nil
+      }
+    })
+  }
+
+  func token() -> Parser<A> {
+    return self.precedes(Parsers.spaces())
+  }
+
+  func precedes<B>(_ follows: Parser<B>) -> Parser<A> {
+    return Parser({ input in 
+      if let (result1, rest1) = self.parse(input) {
+        if let (_, rest2) = follows.parse(rest1) {
+          return (result1, rest2)
+        }
+      }
+
+      return nil
+    })
+  }
+
+  func map<B>(_ mapFunc: @escaping ((A) -> B)) -> Parser<B> {
+    return Parser<B>({ input in
+      if let (result, rest) = self.parse(input) {
+        return (mapFunc(result), rest)
+      } else {
+        return nil
+      }
+    })
+  }
+
+  func flatMap<B>(_ boundParserFunc: @escaping ((A) -> Parser<B>)) -> Parser<B> {
+    return Parser<B>({ input in 
+      if let (result, rest) = self.parse(input) {
+        return boundParserFunc(result).parse(rest)
+      } else {
+        return nil
+      }
+    })
+  }
+
+  func many() -> Parser<[A]> {
+    return Parser<[A]>({ input in
+      var result: [A] = [] 
+      var curInput = input
+      while let (nextResult, nextInput) = self.parse(curInput) {
+        result.append(nextResult)
+        curInput = nextInput
+      }
+
+      return (result, curInput)
+    })
+  }
 }
 
 class Parsers {
+  static func asString(_ parser: Parser<[Character]>) -> Parser<String> {
+    return parser.map(charsToString)
+  }
+
+  static func spaces() -> Parser<Void> {
+    return space().many().then(null())
+  }
+
+  static func space() -> Parser<Character> {
+    return satisfy({ c in c.isWhitespace })
+  }
+
+  static func letters() -> Parser<String> {
+    return asString(letter().many())
+  }
+
+  static func letter() -> Parser<Character> {
+    return satisfy({ c in c.isLetter })
+  }
+
   static func char(_ char: Character) -> Parser<Character> {
     return satisfy({ c in c == char })
   }
@@ -28,6 +130,10 @@ class Parsers {
 
       return nil
     })
+  }
+
+  static func null() -> Parser<Void> {
+    return Parser({ input in ((), input) })
   }
 
   static func strNull(_ str: String) -> Bool {
