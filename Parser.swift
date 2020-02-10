c468066da293bb8a252d7357d3ee49f19e6b0380
Ian Melendez <ian.melendez.95@utexas.edu>
2020-02-09 22:26:54 -0600

    moved html parser example to its own file

--- Parser.swift
+++ Parser.swift
@@ -1,55 +1,3 @@
-class HtmlParserExample {
-  typealias EmptyHtmlTag = (String, [String: String]) // (tag name, attributes)
-
-  static let emptyHtmlTag: Parser<EmptyHtmlTag> = {
-    let openBracket = Parsers.char("<")
-    let attributes: Parser<[String: String]> = 
-      tagAttribute.token().many().map({ attributes in pairsToDict(attributes) })
-    let closeBracket = Parsers.char("/").then(Parsers.char(">"))
-
-    let tagName = Parsers.letters().token()
-
-    return openBracket
-            .then(tagName.flatMap({ tagName in 
-              attributes.precedes(closeBracket).map({ attrs in (tagName, attrs) })
-            }))
-  }()
-
-  static func testEmptyHtmlTag() {
-    print(emptyHtmlTag.parse("<input type=\"text\" value=\"hello world!\"/>"))
-  }
-
-  static func pairsToDict<A,B>(_ pairs: [(A,B)]) -> [A:B] {
-    var dict: [A:B] = [:]
-    for (key, value) in pairs {
-      dict[key] = value
-    }
-    return dict
-  }
-
-  static func testPairsToDict() {
-    print(pairsToDict([("x", 1), ("y", 2)]))
-  }
-
-  static let tagAttribute: Parser<(String, String)> = {
-    let key = Parsers.letters().token()
-
-    let equals = Parsers.char("=")
-    
-    let valueContent = 
-      Parsers.asString(Parsers.satisfy({ c in c != "\"" }).many())
-    let value = valueContent.surroundedBy(Parsers.char("\""))
-
-    return key.flatMap({ keyStr in 
-              equals.then(value.map({ valueStr in (keyStr, valueStr) }))
-            })
-  }()
-
-  static func testTagAttribute() { 
-    print(tagAttribute.parse("type=\"text\""))
-  }
-}
-
 class Parser<A> {
   let parseFunc: ((String) -> ParseResult<A>)
 
@@ -164,7 +112,8 @@ class Parsers {
     if strNull(input) {
       return "Exhausted input"
     } else {
-      return "Failed at char '\(String(strHead(input)!))' in \"\(strTake(input, 20))\""
+      return "Failed at char '\(String(strHead(input)!))'"
+              + " in \"\(strTake(input, 20))\""
     }
   }
 
